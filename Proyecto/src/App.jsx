86488e02d9fb331c966efefd6a774d84e072f56f
Marcos Gerje <31744709+Meglas2017@users.noreply.github.com>
2024-05-17 00:20:01 -0300

    Update App.jsx

--- Proyecto/src/App.jsx
+++ Proyecto/src/App.jsx
@@ -8,7 +8,7 @@ import { Cart } from "./pages/Cart"
 function App() {
 
   return (
-    <BrowserRouter>
+    <BrowserRouter basename="/G10-24303-REACT">
       <FunkosHeader/>
       <Routes>
         <Route path="/" element={<Shop />} />
